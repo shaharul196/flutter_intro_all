b4c0f6ab5026898be2559d1c6ddb3f1b04fddfd5
sazur <sazurana1@gmail.com>
2025-06-27 23:29:34 +0600

    first commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_flutter_sazu/flutter_practise/stack_icon.dart';
-
-import 'module_8/custom_widget.dart';
-import 'module_8/stateful_class.dart';
+import 'assignment_3/api_Crud.dart';
 import 'module_9/aspect_ratio.dart';
-import 'module_9/expanded.dart';
-import 'module_9/layout_builder.dart';
-import 'module_9/mediaquery.dart';
-import 'module_9/todo_app project.dart';
+
 
 
 
@@ -19,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'batch 10',
-      home: Expandedall(),
+      home: Aspectall(),
     );
   }
 }
