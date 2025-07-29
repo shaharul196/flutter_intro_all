65b678360af65ff8d210ccd6f3b70171449283c1
sazur <sazurana1@gmail.com>
2025-07-29 20:38:39 +0600

    first commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,1 @@
-import 'package:flutter/material.dart';
-import 'assignment_3/api_Crud.dart';
-import 'module_9/aspect_ratio.dart';
 
-
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'batch 10',
-      home: Aspectall(),
-    );
-  }
-}
