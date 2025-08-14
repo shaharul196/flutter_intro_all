a3c649520067cfac3d24cb01941d2d80c5d9a711
sazur <sazurana1@gmail.com>
2025-08-14 20:57:36 +0600

    add live score app

--- lib/module_22/firebase_main.dart
+++ lib/module_22/firebase_main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
-import 'app.dart';
+import '../app_1.dart';
 import 'firebase_options.dart';
+import 'app_firebase.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(LiveScoreApp());
 }
