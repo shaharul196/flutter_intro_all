b1876b9cf83aa7e3744ca4299a791a89b604367e
sazur <sazurana1@gmail.com>
2025-09-22 21:37:01 +0600

    add review screen ui

--- lib/module_24/features/products/presentation/screens/review_screen.dart
+++ lib/module_24/features/products/presentation/screens/review_screen.dart
@@ -18,7 +18,64 @@ class _ReviewScreenState extends State<ReviewScreen> {
       appBar: AppBar(title: Text('Reviews')),
       body: Column(
         children: [
-          Expanded(child: Column(children: [])),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: EdgeInsets.all(16),
+                        color: Colors.white,
+                        elevation: 3,
+                        shadowColor: AppColors.themeColor.withOpacity(0.2),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                spacing: 8,
+                                children: [
+                                  SizedBox(
+                                    height: 30,
+                                    width: 30,
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.grey.shade200,
+                                      child: Icon(
+                                        Icons.person,
+                                        color: Colors.grey.shade500,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Sazu Rana',
+                                    style: TextTheme.of(
+                                      context,
+                                    ).bodyLarge?.copyWith(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
 
           TotalReviewAndAddSection(),
         ],
@@ -26,5 +83,3 @@ class _ReviewScreenState extends State<ReviewScreen> {
     );
   }
 }
-
-
