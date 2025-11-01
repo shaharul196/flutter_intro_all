00a56f00b6f91adcac70303618cb7c168e26440e
sazur <sazurana1@gmail.com>
2025-11-01 13:23:23 +0600

    add camera setup

---
+++ lib/module_32_camera/home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CameraController? _cameraController;
+  List<CameraDescription> _cameras = [];
+
+  Future<void> _getCameras() async {
+    _cameras = await availableCameras();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCameras().then((_) {
+      _cameraController = CameraController(_cameras[0], ResolutionPreset.max);
+      _cameraController?.initialize().then((_) {
+        if (mounted) {
+          setState(() {});
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera setup'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body:
+          (_cameraController?.value.isInitialized ?? false)
+              ? Column(
+                children: [
+                  Expanded(child: CameraPreview(_cameraController!)),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FloatingActionButton(
+                            child: Icon(Icons.cameraswitch_rounded),
+                            onPressed: ()  {}
+                        ),
+
+                        FloatingActionButton(
+                          child: Icon(Icons.camera),
+                            onPressed: () async {
+                            XFile? capturedPhoto = await _cameraController?.takePicture();
+                            if(capturedPhoto != null){
+                              print(capturedPhoto.path);
+                            }
+                            }
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+              : Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _cameraController?.dispose();
+  }
+}
---
+++ lib/module_32_camera/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ostad_flutter_sazu/module_32_camera/app.dart';
+
+void main() {
+  return runApp(CameraSetup());
+}
