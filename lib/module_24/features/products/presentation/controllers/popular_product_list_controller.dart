02a7f54a7f60782368e18c08813a7f76040eb3c3
sazur <sazurana1@gmail.com>
2025-11-05 13:36:42 +0600

    add new popular list api

---
+++ lib/module_24/features/products/presentation/controllers/popular_product_list_controller.dart
@@ -0,0 +1,63 @@
+import 'package:get/get.dart';
+import '../../../../app/urls.dart';
+import '../../../../core/models/network_response.dart';
+import '../../../../core/services/network_caller.dart';
+import '../../../shared/data/models/product_model.dart';
+
+class PopularProductListController extends GetxController {
+  int _currentPage = 0;
+  int? _lastPageNo;
+  final int _pageSize = 40;
+
+  bool _getPopularProductListInProgress = false;
+  bool _isInitialLoading = false;
+   final List<ProductModel> _popularProductList = [];
+  String? _errorMessage;
+
+  bool get getPopularProductListInProgress => _getPopularProductListInProgress;
+  bool get isInitialLoading => _isInitialLoading;
+  List<ProductModel> get popularProductList => _popularProductList;
+  String? get errorMessage => _errorMessage;
+
+  Future<bool> getPopularProductList() async {
+    bool isSuccess = false;
+
+    if (_currentPage > (_lastPageNo ?? 1)) {
+      // TODO return korbe mane kicui hobe na
+      return false;
+    }
+    if (_currentPage == 0) {
+      _popularProductList.clear();
+      _isInitialLoading = true;
+    } else {
+      _getPopularProductListInProgress = true;
+    }
+    update();
+    _currentPage++;
+
+    final NetworkResponse response = await Get.find<NetworkCaller>().getRequest(
+      url: Urls.popularProductUrl(_currentPage, _pageSize,),
+    );
+    if (response.isSuccess) {
+      _lastPageNo = response.body!['data']['last_page'];
+      List<ProductModel> list = [];
+      for (Map<String, dynamic> jsonData in response.body!['data']['results']) {
+        list.add(ProductModel.fromJson(jsonData));
+      }
+      // _popularProductList = list;
+      _popularProductList.addAll(list);
+      isSuccess = true;
+      _errorMessage = null;
+    } else {
+      _errorMessage = response.errorMassage;
+    }
+
+    if (_isInitialLoading) {
+      _isInitialLoading = false;
+    } else {
+      _getPopularProductListInProgress = false;
+    }
+    update();
+    return isSuccess;
+  }
+}
